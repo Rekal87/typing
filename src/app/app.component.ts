5f3137a906433220a48d65dc95e66495dd8a2a49
Nnamdi <nnamdi.b@gmail.com>
2023-05-15 18:00:00 +0200

    added conditional display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  userInput = '';
+  randomText = lorem.sentence();
+  compare = false;
+
+  onInput(event: Event) {
+    this.userInput = (event.target as HTMLInputElement).value;
+    if (this.userInput === this.randomText) {
+      this.compare = true;
+    }
+  }
 }
